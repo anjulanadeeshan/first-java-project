3eeb267e4a4ed8a5d2dc150830e4197ffa11340d
anjulanadeeshan <mranjula660@gmail.com>
2025-07-17 15:42:19 +0530

    connect-db

--- src/App.java
+++ src/App.java
@@ -1,11 +1,28 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
     public static void main(String[] args) throws ClassNotFoundException,SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:Supermarket", "root", "200210702007");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "200210702007");
+
+        Statement statement = connection.createStatement();
+
+        String sql = "Select*from Customer";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while(resultSet.next()) {
+            System.out.println(
+                resultSet.getString("CustID")+ " "+ 
+                resultSet.getString("CustName")+" "+ 
+                resultSet.getDouble("salary")
+                );
+        }
+ 
     }
 }
